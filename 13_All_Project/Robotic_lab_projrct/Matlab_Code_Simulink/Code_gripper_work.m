489a29c47e8b7ad6b4ba305f534124a1557d13ad
IMHAMEDBOUJEMAA <imhamed.boujemaa@gmail.com>
2023-02-20 22:30:40 +0100

    Project_add_update

---
+++ 13_All_Project/Robotic_lab_projrct/Matlab_Code_Simulink/Code_gripper_work.m
@@ -0,0 +1,8 @@
+
+
+%execution du fichier XML Simulink
+addpath('C:\Users\bouje\OneDrive\Bureau\Matlab_Gripper');
+smimport('Gripper.XML');
+%en fin il suffit d'executer le Code Created By IMHAMED BOUJEMAA
+
+
---
+++ 13_All_Project/Robotic_lab_projrct/Matlab_Code_Simulink/Gripper_DataFile.m
@@ -0,0 +1,284 @@
+% Simscape(TM) Multibody(TM) version: 7.3
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(26).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(26).angle = 0.0;
+smiData.RigidTransform(26).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(26).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [-41.366196786502556 27.732464926985962 75.874096383475816];  % mm
+smiData.RigidTransform(1).angle = 0;  % rad
+smiData.RigidTransform(1).axis = [0 0 0];
+smiData.RigidTransform(1).ID = 'AssemblyGround[Gripper_finie-1:Base Plate-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [-62.038187163386695 27.732464926985962 38.665141593309002];  % mm
+smiData.RigidTransform(2).angle = 1.2244859314432819;  % rad
+smiData.RigidTransform(2).axis = [0 1 0];
+smiData.RigidTransform(2).ID = 'AssemblyGround[Gripper_finie-1:Gripper-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [-62.038187163369926 21.732464926985962 38.66514159333591];  % mm
+smiData.RigidTransform(3).angle = 1.3373265717747478;  % rad
+smiData.RigidTransform(3).axis = [0 -1 0];
+smiData.RigidTransform(3).ID = 'AssemblyGround[Gripper_finie-1:link-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [-27.866196786509509 21.732464926985877 68.824096383472991];  % mm
+smiData.RigidTransform(4).angle = 1.4549641356266534;  % rad
+smiData.RigidTransform(4).axis = [0 1 0];
+smiData.RigidTransform(4).ID = 'AssemblyGround[Gripper_finie-1:link-4]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [-33.86619678648249 27.232464926985955 89.274096383483453];  % mm
+smiData.RigidTransform(5).angle = 1.7885455147870151;  % rad
+smiData.RigidTransform(5).axis = [5.2497696724328484e-17 -1 2.219118076482095e-16];
+smiData.RigidTransform(5).ID = 'AssemblyGround[Gripper_finie-1:Base Gear-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [-54.866196786496424 30.732464926985962 68.824096383480708];  % mm
+smiData.RigidTransform(6).angle = 1.8042660817804026;  % rad
+smiData.RigidTransform(6).axis = [1.0006185031030351e-16 1 -1.5255706124955604e-16];
+smiData.RigidTransform(6).ID = 'AssemblyGround[Gripper_finie-1:Gear Link 1-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-41.366196786502549 27.732464926985962 100.15409638347579];  % mm
+smiData.RigidTransform(7).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(7).axis = [0.70710678118654768 0 0.70710678118654746];
+smiData.RigidTransform(7).ID = 'AssemblyGround[Gripper_finie-1:Base Mounting Bracket-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-41.366196786502542 30.732464926985966 100.15409638347582];  % mm
+smiData.RigidTransform(8).angle = 1.5707963267948968;  % rad
+smiData.RigidTransform(8).axis = [-0 -1 -0];
+smiData.RigidTransform(8).ID = 'AssemblyGround[Gripper_finie-1:Base Mounting Bracket-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [-46.616196786501796 30.732464926985962 47.93331344453604];  % mm
+smiData.RigidTransform(9).angle = 1.8302970885919576;  % rad
+smiData.RigidTransform(9).axis = [0 1 0];
+smiData.RigidTransform(9).ID = 'AssemblyGround[Gripper_finie-1:link-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [-62.038187163386695 24.732464926985962 38.665141593309002];  % mm
+smiData.RigidTransform(10).angle = 1.2244859314432819;  % rad
+smiData.RigidTransform(10).axis = [0 1 0];
+smiData.RigidTransform(10).ID = 'AssemblyGround[Gripper_finie-1:Gripper-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [-24.283423155628 27.732464926985909 38.03182895950642];  % mm
+smiData.RigidTransform(11).angle = 3.1415926535897913;  % rad
+smiData.RigidTransform(11).axis = [-0.57846975963327185 4.953241197411902e-16 0.81570382933379981];
+smiData.RigidTransform(11).ID = 'AssemblyGround[Gripper_finie-1:Gripper-4]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [-54.570736679949306 24.732464926985962 17.971249178475411];  % mm
+smiData.RigidTransform(12).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(12).axis = [0.79265315904213562 0 0.60967283805375716];
+smiData.RigidTransform(12).ID = 'AssemblyGround[Gripper_finie-1:link-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [-27.866196786502599 20.732464926985941 68.824096383475819];  % mm
+smiData.RigidTransform(13).angle = 0.00088672805999003761;  % rad
+smiData.RigidTransform(13).axis = [0 1 0];
+smiData.RigidTransform(13).ID = 'AssemblyGround[Gripper_finie-1:pin-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(14).translation = [-54.866196786502556 20.732464926985962 68.824096383475819];  % mm
+smiData.RigidTransform(14).angle = 0.53275950077095224;  % rad
+smiData.RigidTransform(14).axis = [0 1 0];
+smiData.RigidTransform(14).ID = 'AssemblyGround[Gripper_finie-1:pin-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(15).translation = [-46.616196786502556 20.732464926985948 47.933313444535287];  % mm
+smiData.RigidTransform(15).angle = 0.53740235593481156;  % rad
+smiData.RigidTransform(15).axis = [0 1 0];
+smiData.RigidTransform(15).ID = 'AssemblyGround[Gripper_finie-1:pin-4]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(16).translation = [-36.116196786503828 24.732464926985902 47.933313444534342];  % mm
+smiData.RigidTransform(16).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(16).axis = [0.75729115288249083 0 -0.65307741483373016];
+smiData.RigidTransform(16).ID = 'AssemblyGround[Gripper_finie-1:link-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(17).translation = [-27.866196786509825 30.732464926985962 68.824096383472877];  % mm
+smiData.RigidTransform(17).angle = 1.4549641355920144;  % rad
+smiData.RigidTransform(17).axis = [6.1083229919755943e-16 1 -2.2164556779754702e-15];
+smiData.RigidTransform(17).ID = 'AssemblyGround[Gripper_finie-1:Gear Link 1-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(18).translation = [-36.116196786502563 20.73246492698598 47.933313444535287];  % mm
+smiData.RigidTransform(18).angle = 0;  % rad
+smiData.RigidTransform(18).axis = [0 0 0];
+smiData.RigidTransform(18).ID = 'AssemblyGround[Gripper_finie-1:pin-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(19).translation = [-24.283423155627993 30.732464926985909 38.03182895950642];  % mm
+smiData.RigidTransform(19).angle = 3.1415926535897913;  % rad
+smiData.RigidTransform(19).axis = [-0.57846975963327185 4.9534724987381919e-16 0.81570382933379981];
+smiData.RigidTransform(19).ID = 'AssemblyGround[Gripper_finie-1:Gripper-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(20).translation = [-36.116196786504048 30.732464926985962 47.933313444534178];  % mm
+smiData.RigidTransform(20).angle = 1.4232821325445755;  % rad
+smiData.RigidTransform(20).axis = [1.3803174338212055e-15 1 -9.5543043026512821e-16];
+smiData.RigidTransform(20).ID = 'AssemblyGround[Gripper_finie-1:link-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(21).translation = [-54.570736679949619 20.732464926985962 17.971249178474466];  % mm
+smiData.RigidTransform(21).angle = 1.5020854022783787;  % rad
+smiData.RigidTransform(21).axis = [0 1 0];
+smiData.RigidTransform(21).ID = 'AssemblyGround[Gripper_finie-1:pin-8]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(22).translation = [-62.038187163373692 20.732464926985962 38.665141593329857];  % mm
+smiData.RigidTransform(22).angle = 0.79186248878683507;  % rad
+smiData.RigidTransform(22).axis = [0 1 0];
+smiData.RigidTransform(22).ID = 'AssemblyGround[Gripper_finie-1:pin-9]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(23).translation = [-41.366196786502549 24.732464926985962 75.874096383475816];  % mm
+smiData.RigidTransform(23).angle = 0;  % rad
+smiData.RigidTransform(23).axis = [0 0 0];
+smiData.RigidTransform(23).ID = 'AssemblyGround[Gripper_finie-1:Base Plate-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(24).translation = [-24.283423155648617 20.732464926985877 38.031828959493239];  % mm
+smiData.RigidTransform(24).angle = 0.63261926374818822;  % rad
+smiData.RigidTransform(24).axis = [0 -1 0];
+smiData.RigidTransform(24).ID = 'AssemblyGround[Gripper_finie-1:pin-7]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(25).translation = [-31.559823591989666 20.732464926985902 17.269989043676144];  % mm
+smiData.RigidTransform(25).angle = 0;  % rad
+smiData.RigidTransform(25).axis = [0 0 0];
+smiData.RigidTransform(25).ID = 'AssemblyGround[Gripper_finie-1:pin-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(26).translation = [32.4129569609971 -30.224893614690679 85.20745113266041];  % mm
+smiData.RigidTransform(26).angle = 0;  % rad
+smiData.RigidTransform(26).axis = [0 0 0];
+smiData.RigidTransform(26).ID = 'RootGround[Gripper_finie-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(7).mass = 0.0;
+smiData.Solid(7).CoM = [0.0 0.0 0.0];
+smiData.Solid(7).MoI = [0.0 0.0 0.0];
+smiData.Solid(7).PoI = [0.0 0.0 0.0];
+smiData.Solid(7).color = [0.0 0.0 0.0];
+smiData.Solid(7).opacity = 0.0;
+smiData.Solid(7).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.0041405530951580454;  % kg
+smiData.Solid(1).CoM = [-1.130925292278997 1.5 -0.87608041695238903];  % mm
+smiData.Solid(1).MoI = [1.3494746590459741 1.9689326223286909 0.62566879292545419];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0.036700467186905367 0];  % kg*mm^2
+smiData.Solid(1).color = [0.7803921568627451 0.76078431372549016 0.74117647058823533];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'Base Plate*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.0015997032514136612;  % kg
+smiData.Solid(2).CoM = [24.608434200351525 1.5000000000000002 -3.6825247387653195];  % mm
+smiData.Solid(2).MoI = [0.040435661754353841 0.49479040271715824 0.45675429583992488];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0.10015750409834784 0];  % kg*mm^2
+smiData.Solid(2).color = [0.7803921568627451 0.76078431372549016 0.74117647058823533];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = 'Gripper*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.00040952356757313716;  % kg
+smiData.Solid(3).CoM = [15.499999999999995 1.5 0];  % mm
+smiData.Solid(3).MoI = [0.0011630543760508881 0.057925644844933392 0.05737558123231485];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(3).color = [0.7803921568627451 0.76078431372549016 0.74117647058823533];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = 'link*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.0023925888882278036;  % kg
+smiData.Solid(4).CoM = [0 2.4777373132421761 0];  % mm
+smiData.Solid(4).MoI = [0.071141783523596958 0.081168341185431134 0.071141783523596694];  % kg*mm^2
+smiData.Solid(4).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(4).color = [0.7803921568627451 0.76078431372549016 0.74117647058823533];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = 'Base Gear*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 0.0020768147601832733;  % kg
+smiData.Solid(5).CoM = [5.4888490263503327 1.5 2.1492850121701073e-05];  % mm
+smiData.Solid(5).MoI = [0.073729244633828392 0.36111390254462306 0.29049987989368065];  % kg*mm^2
+smiData.Solid(5).PoI = [0 -3.5217322699144975e-07 0];  % kg*mm^2
+smiData.Solid(5).color = [0.7803921568627451 0.76078431372549016 0.74117647058823533];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = 'Gear Link 1*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(6).mass = 0.00064269578502266307;  % kg
+smiData.Solid(6).CoM = [7.7303000078989266 2.4139895028559866 0];  % mm
+smiData.Solid(6).MoI = [0.023626413362400747 0.028788298455440023 0.01118597853161905];  % kg*mm^2
+smiData.Solid(6).PoI = [0 0 -0.0029678789233181777];  % kg*mm^2
+smiData.Solid(6).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(6).opacity = 1;
+smiData.Solid(6).ID = 'Base Mounting Bracket*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(7).mass = 0.00012409290981679687;  % kg
+smiData.Solid(7).CoM = [0 7.0000000000000018 0];  % mm
+smiData.Solid(7).MoI = [0.0027893743022295151 0.00021814433988364144 0.0027893743022295151];  % kg*mm^2
+smiData.Solid(7).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(7).color = [0.7803921568627451 0.76078431372549016 0.74117647058823533];
+smiData.Solid(7).opacity = 1;
+smiData.Solid(7).ID = 'pin*:*Default';
+
